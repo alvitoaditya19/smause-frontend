fe0c6e30b9f786bf139650713281cf0a66f60740
alvitoaditya19 <you@example.com>
2023-03-02 12:24:11 +0700

    revision

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ['localhost'] },
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
 }
 
 module.exports = nextConfig
